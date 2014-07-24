3c94fc7de76b85b3d3f522e50b865ef656dc44d7
Kyle Rush <kylerrush@gmail.com>
2014-07-24 17:22:46 -0400

    More scaffolding.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,10 +2,83 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
 
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      gruntfile: {
+        options: {
+          trailing: true,
+          curly: true,
+          eqeqeq: true,
+          indent: 4,
+          latedef: true,
+          noempty: true,
+          nonbsp: true,
+          undef: true,
+          unused: true,
+          quotmark: 'single',
+          browser: true,
+          globals: {
+            module: false
+          }
+        },
+        files: {
+          src: ['Gruntfile.js']
+        }
+      },
+      oform: {
+        options: {
+          trailing: true,
+          curly: true,
+          eqeqeq: true,
+          indent: 4,
+          latedef: true,
+          noempty: true,
+          nonbsp: true,
+          undef: true,
+          unused: true,
+          quotmark: 'single',
+          browser: true,
+          globals: {
+            jQuery: true,
+            $: true
+          }
+        },
+        files: {
+          src: ['oForm.js']
+        }
+      }
+    },
+    jsonlint: {
+      src: ['package.json', 'bower.json']
+    },
+    uglify: {
+      options: {
+        mangle: false,
+        compress: true,
+        wrap: true
+      },
+      oform: {
+        options: {
+          banner: '/* oForm - Author: Kyle Rush - MIT license - https://github.com/kylerush/oform */ \n'
+        },
+        files: {
+          'dist/oForm.min.js': ['oForm.js']
+        }
+      }
+    }
 
   });
 
-  runt.registerTask('default', []);
+  grunt.loadNpmTasks('grunt-jsonlint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
-);
+  grunt.registerTask('build', [
+    'jshint',
+    'jsonlint',
+    'uglify'
+  ]);
+
+  grunt.registerTask('default', []);
+
+};
